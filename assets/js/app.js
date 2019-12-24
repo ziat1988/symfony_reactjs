4a9efdccbb6e7387a4746e0e4d6629874a519c20
DESKTOP-DI3TG5E\dang <dang@resto-pro.com>
2019-12-24 15:24:20 +0100

    set up init

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+            <p>Hello there. Name you???. Met moi qua di</p>
+            </div>
+    )
+    }
+}
+ReactDOM.render(<App/>, document.getElementById('app'));
